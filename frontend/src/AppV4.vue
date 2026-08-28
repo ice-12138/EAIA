4ff11fa9f1fece5d9dcca94cb6ba4e436e56b7ba
ice-12138 <56675757+ice-12138@users.noreply.github.com>
2026-08-28 20:13:28 +0800

    Route all pages through unified workspace

--- frontend/src/AppV4.vue
+++ frontend/src/AppV4.vue
@@ -1,15 +1,14 @@
 <script setup>
 import { computed, onMounted, onUnmounted, ref } from 'vue'
-import DataManager from './DataManager.vue'
+import UnifiedWorkspace from './UnifiedWorkspace.vue'
 
 const hash = ref(window.location.hash)
 function syncHash() { hash.value = window.location.hash }
 onMounted(() => window.addEventListener('hashchange', syncHash))
 onUnmounted(() => window.removeEventListener('hashchange', syncHash))
-
 const route = computed(() => hash.value.replace(/^#\/?/, '') || 'heroes')
 </script>
 
 <template>
-  <DataManager :mode="route" />
+  <UnifiedWorkspace :mode="route" />
 </template>
